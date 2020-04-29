31e82e031711f976587bfb12d09326495c2a21b4
ArchitNegi <architnegi1999@gmail.com>
2020-04-30 00:15:04 +0530

    Kaden's Algo

---
+++ LargestSumSubArray.java
@@ -0,0 +1,28 @@
+package aa;
+import java.util.*;
+public class LargestSumSubArray {
+	public static void main(String[]args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of elements");
+		int n=sc.nextInt();
+		System.out.println("Enter the elements of the array");
+		int[]arr=new int[n];
+		int cursum=0;
+		int maxsum=Integer.MIN_VALUE;
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		sc.close();
+		for(int i=0;i<n;i++) {
+			if(maxsum<cursum) {
+				maxsum=cursum;
+			}
+			if(cursum<0) {
+				cursum=0;
+			}
+			
+		}
+		System.out.println(maxsum);
+	}
+}
+ 
